c5f2186439b54506c1cdbf5c212c4cc1e13c9aa9
Harsh Agarwal <harshagarwal8002@gmail.com>
2024-05-31 08:08:30 +0530

    Adding promises

--- promises.js
+++ promises.js
@@ -1,19 +1,47 @@
-let getpromise = (id)=>{
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            if (id =21){
-            resolve("Data is found")
-            }
-            else{
-                reject("Data is not present")
-            }
-    },10000)
-});}
+// let getpromise = (id)=>{
+//     return new Promise((resolve, reject) => {
+//         setTimeout(()=>{
+//             if (id =21){
+//             resolve("Data is found")
+//             }
+//             else{
+//                 reject("Data is not present")
+//             }
+//     },10000)
+// });}
 
-let promis = getpromise(21);
-promis.then((res)=>{
-    console.log(res)
-})
-promis.catch((err)=>{
-    console.log(err)
-});
+// let promis = getpromise(21);
+// promis.then((res)=>{
+//     console.log(res)
+// })
+// promis.catch((err)=>{
+
+//     console.log(err)
+// });
+
+
+let getdata = (data)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("founddata", data)
+            reject("Success")
+        },2000)
+    })
+}
+
+// getdata(1).then((res)=>{
+//     return getdata(2).then(()=>{
+//         return getdata(3).then(()=>{
+//             console.log(res)
+//         })
+//     })
+// })
+
+ let data=  async ()=>{
+    await getdata(1);
+    await getdata(2);
+    await getdata(3);
+    await getdata(4);    
+}
+
+data();
